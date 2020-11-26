8879f58873c1acd8adcd2caedc0430525d622e6a
Lars Kemmann <lkemmann@netrixllc.com>
2020-11-26 12:30:51 -0500

    Starting point for coding exercise
    Added React+Redux+API web project
    Added F# model
    Added party controller with in-memory sample data

---
+++ QuestTracker.Core/Model.fs
@@ -0,0 +1,48 @@
+﻿namespace QuestTracker
+
+open System
+
+module Model =
+
+    type Adventurer = {
+        Name : string
+        Skills: string seq
+    }
+
+    type Encounter = {
+        Timestamp : DateTime
+        Description : string
+    }
+
+    type PartyState =
+    | Forming = 0
+    | Active = 1
+    | Complete = 2
+
+    type Party = {
+        State : PartyState
+        Adventurers : Adventurer seq
+        Encounters : Encounter seq
+    }
+
+    let formParty() = { State = PartyState.Forming; Adventurers = Seq.empty; Encounters = Seq.empty }
+
+    let addAdventurer party adventurer =
+        match party.State with
+        | PartyState.Forming -> { party with Adventurers = Seq.append party.Adventurers [adventurer] }
+        | _ -> failwith "Adventurers can only be added to a party that is forming"
+
+    let activateQuest party =
+        match party.State with
+        | PartyState.Forming -> { party with State = PartyState.Active }
+        | _ -> failwith "A party can only activate a quest when forming"
+        
+    let addEncounter party encounter =
+        match party.State with
+        | PartyState.Active -> { party with Encounters = Seq.append party.Encounters [encounter] }
+        | _ -> failwith "A party can only have encounters when it is active"
+
+    let completeQuest party =
+        match party.State with
+        | PartyState.Active -> { party with State = PartyState.Complete }
+        | _ -> failwith "A party can only complete its quest when active"
---
+++ QuestTracker.Web/Controllers/PartyController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using static QuestTracker.Model;
+
+namespace QuestTracker.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PartyController : ControllerBase
+    {
+        // GET: api/<PartyController>
+        [HttpGet]
+        public IEnumerable<string> GetPartyNames()
+        {
+            return parties.Keys;
+        }
+
+        // GET api/<PartyController>/ABC
+        [HttpGet("{partyName}")]
+        public Party GetParty(string partyName)
+        {
+            return parties[partyName];
+        }
+
+        // POST api/<PartyController>/ABC/new
+        [HttpPost("{partyName}/new")]
+        public void FormNewParty(string partyName)
+        {
+            parties.Add(partyName, formParty());
+        }
+
+        // POST api/<PartyController>/ABC/addAdventurer
+        [HttpPost("{partyName}/addAdventurer")]
+        public void AddAdventurer(string partyName, [FromBody] Adventurer adventurer)
+        {
+            var party = parties[partyName];
+            var updatedParty = addAdventurer(party, adventurer);
+            parties[partyName] = updatedParty;
+        }
+
+        // DELETE api/<PartyController>/5
+        [HttpDelete("{partyName}")]
+        public void DeleteParty(string partyName)
+        {
+            parties.Remove(partyName);
+        }
+
+
+        private Dictionary<string, Party> parties = new Dictionary<string, Party>
+        {
+            ["Colonize Mars"] = new Party(PartyState.Forming,
+                new List<Adventurer>
+                {
+                    new Adventurer("Elon Musk", new List<string> { "Visionary" })
+                }, new List<Encounter>()),
+            ["Fellowship of the Ring"] = new Party(PartyState.Active,
+                new List<Adventurer>
+                {
+                    new Adventurer("Frodo Baggins", new List<string> { "Walking", "Carrying Rings" }),
+                    new Adventurer("Samwise Gamgee", new List<string> { "Cooking", "Bravery" })
+                },
+                new List<Encounter>()),
+            ["Oregon Trail"] = new Party(PartyState.Complete,
+                new List<Adventurer>
+                {
+                    new Adventurer("John Cooper", new List<string> { "Hunting", "Building Rafts", "Setting Rations" }),
+                    new Adventurer("Sandy Cooper", new List<string> { "Cooking", "Dying of Dysentery" }),
+                    new Adventurer("Sally Cooper", new List<string> { "Eating", "Dying of Typhoid" })
+                },
+                new List<Encounter>
+                {
+                    new Encounter(new DateTime(1841, 3, 5), "You have set out from Independence"),
+                    new Encounter(new DateTime(1841, 3, 15), "Sally Cooper has typhoid"),
+                    new Encounter(new DateTime(1841, 4, 1), "Sally Cooper died of typhoid"),
+                    new Encounter(new DateTime(1841, 4, 3), "Sandy Cooper has dysentery"),
+                    new Encounter(new DateTime(1841, 4, 5), "Sandy Cooper died of dysentery"),
+                    new Encounter(new DateTime(1841, 4, 15), "John Cooper drowned while trying to raft across the South Platte River"),
+                })
+        };
+    }
+}
